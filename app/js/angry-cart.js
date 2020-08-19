60b902951dffac749d09e70c1888d9a1950e3224
Rah Emil <013131@mail.ru>
2020-08-19 12:48:58 +0300

    Publish v=2.0.0

--- app/js/angry-cart.js
+++ app/js/angry-cart.js
@@ -1,48 +1,93 @@
+/**
+ * angry-cart.js 2.0.0
+ * angry-cart.js - JavaScript shopping cart plugin
+ * GitHub: https://github.com/rah-emil/angry-cart.js
+ *
+ * Copyright 2020 Rah Emil
+ *
+ * Released under the MIT License
+ *
+ * Released: August 18, 2020
+ * Latest updates: August 19, 2020
+ */
+
+
 const angryCartKey = 'angry-cart';
 
 class AngryCart {
 
 	constructor(options) {
 		this.$counter = document.querySelector(options.counter),
-		this.$cartPreview = document.querySelector(options.cartPreview),
+		this.$totalPrice = document.querySelectorAll(options.totalPrice),
 		this.__init()
 	}
 
 	__init(){
 		if( localStorage.getItem(angryCartKey) )
 		{
-			let angryCartData = JSON.parse(localStorage.getItem(angryCartKey)) ?? [];
+			let angryCartData = JSON.parse(localStorage.getItem(angryCartKey));
 			this.$counter.innerHTML = angryCartData.length;
-
-			this.render(angryCartData);
+			this.__save(angryCartData);
 		}
 		else
 		{
-			localStorage.setItem(angryCartKey, null);
+			localStorage.setItem(angryCartKey, JSON.stringify([]));
 		}
 	}
 
 	__save(angryCartData){
 		localStorage.setItem(angryCartKey, JSON.stringify(angryCartData));
+		this.__calcTotal();
+		this.outputCart();
 	}
 
-	render(angryCartData){
-		this.$cartPreview.innerHTML = '';
+	__calcTotal(){
+		try
+		{
+			if(this.$totalPrice.length != 0){
+				let angryCartData = JSON.parse( localStorage.getItem(angryCartKey) ),
+					total = 0;
 
-		Array.from(angryCartData ?? []).forEach( (product) => {
-			this.$cartPreview.insertAdjacentHTML('beforeEnd', `<h6>*Имя товара*</h6><span>id: ${product.product_id}, кол-во: ${product.quantity}</span><button class="btn btn-sm btn-danger" onclick="cart.removeFromCart(${product.product_id})">x</button><hr/>`);
-		});
+				Array.from(angryCartData ?? []).forEach( (product) => {
+					let price = product.data.price,
+						quantity = product.quantity;
+
+					total += price * quantity;
+				});
+
+				Array.from(this.$totalPrice).forEach( (totalPrice) => {
+					totalPrice.innerHTML = total.toFixed(2);
+				});
+			}
+		}
+		catch(error)
+		{
+			console.warn('No price for this item');
+			localStorage.setItem(angryCartKey, JSON.stringify([]));
+		}
 	}
 
-	addToCart(product_id){
-		let angryCartData = JSON.parse( localStorage.getItem(angryCartKey) ) ?? [],
+	outputCart(){
+		try
+		{
+			angryCPR();
+		}
+		catch(error)
+		{
+			console.warn('Function angryCPR() is not defined');
+		}
+	}
+
+	addToCart(product_id, data){
+		let angryCartData = JSON.parse( localStorage.getItem(angryCartKey) ) ?? null,
 			existingProducts = angryCartData.find(product => product.product_id === product_id);
 
 		if( !existingProducts )
 		{
 			angryCartData.push({
 				product_id: product_id,
-				quantity: 1
+				quantity: 1,
+				data: data
 			});
 		}
 		else
@@ -50,26 +95,26 @@ class AngryCart {
 			existingProducts.quantity++;
 		}
 
+
 		this.__save(angryCartData);
 		this.$counter.innerHTML = angryCartData.length;
-		this.render(angryCartData);
 	}
 
 	removeFromCart(product_id){
-		let angryCartData = JSON.parse(localStorage.getItem(angryCartKey)) ?? [],
+		let angryCartData = JSON.parse(localStorage.getItem(angryCartKey) ?? null),
 			existingProducts = angryCartData.findIndex(product => product.product_id === product_id);
 
 		angryCartData.splice( existingProducts, 1 );
 
 		this.__save(angryCartData);
 		this.$counter.innerHTML = angryCartData.length;
-		this.render(angryCartData);
 	}
 
 	clearCart(){
-		localStorage.setItem(angryCartKey, null);
+		localStorage.setItem(angryCartKey, JSON.stringify([]));
 		this.$counter.innerHTML = 0;
-		this.$cartPreview.innerHTML = '';
+		this.__calcTotal();
+		this.outputCart();
 	}
 
 }
